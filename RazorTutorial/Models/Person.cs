a48524b4fe7028d318bc053e024df86af22133f4
Brian <brianezequielv2004@gmail.com>
2024-05-24 14:56:40 -0300

    first commit

---
+++ RazorTutorial/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace RazorTutorial.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
